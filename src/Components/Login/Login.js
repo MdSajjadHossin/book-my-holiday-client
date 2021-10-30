14c3805f8a6bb1449b9f396afe3fe36c397781d6
MdSajjadHossin <iamsajjadhossin@gmail.com>
2021-10-31 00:35:13 +0600

    final push

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
     return (
         <div>
             <h2> Please Login </h2>
-            <button onClick= {handleGoogleLogin} className="btn-primary">Coogle Sign In</button>
+            <button onClick= {handleGoogleLogin} className="btn-danger rounded-3 m-5 p-2">Coogle Sign In</button>
         </div>
     );
 };
--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import notFound from '../../Images/notfound.jpg';
 
 const NotFound = () => {
     return (
         <div>
-            <h2>Page Not Found</h2>
+            <img className="w-50" src={notFound} alt="" /><br />
+            <Link to="/">
+            <button className="btn-danger m-3 p-2 rounded">Go Home</button>
+            </Link>
         </div>
     );
 };
