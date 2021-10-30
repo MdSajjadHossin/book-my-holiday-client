d03bd46cd8837aea5545f6ba808ec321392f953f
MdSajjadHossin <iamsajjadhossin@gmail.com>
2021-10-30 16:51:43 +0600

    product added

--- src/Components/AddItem/AddItem.js
+++ src/Components/AddItem/AddItem.js
@@ -25,9 +25,9 @@ const AddItem = () => {
         <div>
             <h3>This is add Item</h3>
             <form className="shipping-form" onSubmit={handleSubmit(onSubmit)}>
-            <input placeholder="Product Name" defaultValue="" {...register("productName")} />
-            <input placeholder="Image URL" defaultValue="" {...register("imageURL")} />
-            <input placeholder="Cost" defaultValue="" {...register("cst")} />
+            <input placeholder="Product Name" defaultValue="" {...register("name")} />
+            <input placeholder="Image URL" defaultValue="" {...register("serviceImg")} />
+            <input placeholder="Cost" defaultValue="" {...register("cost")} />
             <textarea placeholder="Description" defaultValue="" {...register("description")} />
 
             <input type="submit" />
